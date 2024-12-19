bc9f5a77af22a6f39e58d698d8ff1911f05db160
Maushmi Panda <mousumipanda946@gmail.com>
2024-12-19 20:06:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './index.css';
-import Home from "./component/Home"
-import AboutMe from './component/About';
+import Home from "./component/Home";
+import AboutMe from "./Component/About"
 import Skills from './component/Skill';
 import Project from './component/Project';
 import Contact from './component/Contact';
